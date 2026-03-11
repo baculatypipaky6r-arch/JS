2a4c432351728606be36fe8048ed6317001fbd38
baculatypipaky6r-arch <baculaty_pipaky6r@icloud.com>
2026-03-11 11:39:30 +0100

    update

---
+++ 3.3.4/3.3.4/cvičení/script.js
@@ -0,0 +1,10 @@
+let age2 = 18;
+let fullprice = 100;
+
+
+if (age2 < 3) {
+    console.log("Cema pro dítě do tří let je" + fullprice * 0 + "Kč");
+} else if(age2 >= 3 & age < 12){
+    console.log ("Vaše cena je" + fullprice * 0,7 + "Kč");
+} else (age2 > 12) 
+    console.log("vaše cena" + fullprice + "Kč");
