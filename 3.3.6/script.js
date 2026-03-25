fd5f8a69c82c5a9ddbc5398f24ec12ed2c7908b9
baculatypipaky6r-arch <baculaty_pipaky6r@icloud.com>
2026-03-25 11:34:20 +0100

    update

---
+++ 3.3.6/script.js
@@ -0,0 +1,23 @@
+let counter = 0;
+
+while (counter < 10) {
+    console.log(counter);
+    counter++;
+}
+
+let i = 1;
+
+while (i <= 10) {
+    let message = i
+
+    if (i === 7){
+        console.log("7 is my lucky number!")
+    }
+
+    console.log(message);
+    i++;
+}
+
+for (let a = 0; a < 10; a++){
+    console.log(a);
+}
